1ed4512c2b44139cd10e3c99755ef29beb4c982c
Artem Vavilov <artem.vavilov.7@gmail.com>
2021-04-07 15:49:41 +0300

    Fix AutoAccessory.xsd

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe AmazonFeedValidator do
   it 'has a version number' do
-    expect(AmazonFeedValidator::VERSION).to eql '0.4.0'
+    expect(AmazonFeedValidator::VERSION).to eql '0.4.1'
   end
 end
